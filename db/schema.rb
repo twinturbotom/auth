f6add6bd307568b1545b9734ea5ae0df8f02bfd3
twinturbotom <tommmello@gmail.com>
2012-12-10 22:56:43 -0500

    users scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,10 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121210230541) do
+ActiveRecord::Schema.define(:version => 20121211035430) do
 
   create_table "users", :force => true do |t|
-    t.string   "email"
+    t.string   "name"
     t.string   "password"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
