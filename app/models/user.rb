b05260cc40c91c60327c028a83f0d256fdc007f9
twinturbotom <tommmello@gmail.com>
2012-12-10 23:18:59 -0500

    users from scaffold

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :password
+  attr_accessible :email, :password
+
+  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,10 +14,10 @@
 ActiveRecord::Schema.define(:version => 20121211035430) do
 
   create_table "users", :force => true do |t|
-    t.string   "name"
-    t.string   "password"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
 end
